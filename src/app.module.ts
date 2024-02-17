35800217282c99bb2e2773e559f454b48f03baa9
duy.hoang <duy.hoang@smartr.co>
2024-02-17 21:47:11 +0700

    feat/connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { RoomsModule } from './participants/rooms/rooms.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ParticipantsModule } from './participants/participants.module';
 import { MessagesModule } from './messages/messages.module';
+import { DatabaseModule } from './database.module';
+import * as Joi from '@hapi/joi';
+import {ConfigModule} from '@nestjs/config'
 
 @Module({
-  imports: [UsersModule, RoomsModule, ParticipantsModule, MessagesModule],
+  imports: [
+    UsersModule, 
+    RoomsModule, 
+    ParticipantsModule, 
+    MessagesModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.usersRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.usersRepository.findOneBy({
+      id: id
+    })
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
