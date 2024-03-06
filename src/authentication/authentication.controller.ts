21fb6c28657933815b4431af292a667055684b45
duy.hoang <duy.hoang@smartr.co>
2024-03-06 20:05:24 +0700

    authentication

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { RegisterDto } from './dto/register.dto';
+import { LocalAuthenticationGuard } from './localAuthentication.guard';
+import RequestWithUser from './requestWithUser.interface';
+import { JwtAuthenticationGuard } from './jwt-authentication.guard';
+import { Response } from 'express';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('register')
+  async register(@Body() registrationDto: RegisterDto) {
+    return this.authenticationService.register(registrationDto);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthenticationGuard)
+  @Post('log-in')
+  async LogIn(@Req() request: RequestWithUser, @Res() response: Response) {
+    const { user } = request;
+    const cookie = this.authenticationService.getCookieWithJwtToken(user.id);
+    user.password = undefined;
+    response.setHeader('Set-Cookie', cookie);
+    return response.send(user);
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Post('log-out')
+  async LogOut(@Req() request: RequestWithUser, @Res() response: Response) {
+    response.setHeader(
+      'Set-Cookie',
+      this.authenticationService.getCookieForLogOut(),
+    );
+    return response.sendStatus(200);
+  }
+}
